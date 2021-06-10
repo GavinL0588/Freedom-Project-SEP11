44c9dcbf395c0db7938eab5976377af7fbf8e45a
Gavin Lee <GavinL0588@hstat.org>
2021-06-10 16:15:29 +0000

    update repo

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 
 document.querySelector("#test").addEventListener("keypress",function(event){
-   if(event.target.value=="Flip"){
-       document.querySelector("p").style.display="block";
-   }  if(event.target.value=="Flash"){
+     if(event.target.value=="Flash"){
       document.querySelector("h4").style.display="block";
    }  if(event.target.value=="Bounce"){
-      document.querySelector("p").style.display="block";
+      document.querySelector("h7").style.display="block";
    }  if(event.target.value=="Swing"){
       document.querySelector("h2").style.display="block";
    }
@@ -14,11 +12,53 @@ document.querySelector("#test").addEventListener("keypress",function(event){
    }
     if(event.target.value=="Heart Beat"){
       document.querySelector("h5").style.display="block";
+   }if(event.target.value=="Zoom In"){
+      document.querySelector("h6").style.display="block";
+   }if(event.target.value=="Fade In"){
+      document.querySelector("h8").style.display="block";
+   }if(event.target.value=="Light Speed"){
+      document.querySelector("h9").style.display="block";
+   }if(event.target.value=="Slide Up"){
+      document.querySelector("h10").style.display="block";
+   }if(event.target.value=="Roll Out"){
+      document.querySelector("h11").style.display="block";
    }
 });
+
+
+document.querySelector("#test").addEventListener("keypress",function(event){
+  if(event.target.value=="2"){
+      document.querySelector("h4").style.display="block";
+   }  if(event.target.value=="1"){
+      document.querySelector("h7").style.display="block";
+   }  if(event.target.value=="3"){
+      document.querySelector("h2").style.display="block";
+   }
+    if(event.target.value=="4"){
+      document.querySelector("h3").style.display="block";
+   }
+    if(event.target.value=="5"){
+      document.querySelector("h5").style.display="block";
+   }if(event.target.value=="6"){
+      document.querySelector("h6").style.display="block";
+   }if(event.target.value=="7"){
+      document.querySelector("h8").style.display="block";
+   }if(event.target.value=="8"){
+      document.querySelector("h9").style.display="block";
+   }if(event.target.value=="9"){
+      document.querySelector("h10").style.display="block";
+   }if(event.target.value=="0"){
+      document.querySelector("h11").style.display="block";
+   }
+});
+
+
+
+
 
    function Refresh(){
       if(confirm){
          location.reload();
       }
    }
+
